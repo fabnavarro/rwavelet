31a82b7e6aaa1be6f2efb52affbb5b435dd614ff
fabnavarro <fabien.navarro@ensai.fr>
2018-03-07 15:36:19 +0100

    add example

--- R/MAD.R
+++ R/MAD.R
@@ -5,9 +5,8 @@
 #' @export MAD
 #' @param x 1-d signal.
 #' @examples
-#' \dontrun{
+#' x <- c(1, 1, 2, 2, 4, 6, 9)
 #' MAD(x)
-#' }
 
 MAD <- function(x) {
   return(median(abs(x - median(x)))/0.6745)
