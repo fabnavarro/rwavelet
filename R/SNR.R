96b9d33c6731546a9611ab89934f53f6967b8249
fabnavarro <fabien.navarro@ensai.fr>
2018-03-07 16:06:22 +0100

    add example

--- R/SNR.R
+++ R/SNR.R
@@ -5,9 +5,10 @@
 #' @param y Restored or noisy signal
 #' @return Signal/Noise ratio.
 #' @examples
-#' \dontrun{
-#' value=SNR(x,y)
-#' }
+#' n <- 2^4
+#' x <- MakeSignal('HeaviSine', n)
+#' y <- x + rnorm(n, mean=0, sd=1)
+#' SNR(x, y)
 
 SNR <- function(x, y) {
   v <- 20 * log10(normvec(as.vector(x))/normvec(as.vector(x) - as.vector(y)))
