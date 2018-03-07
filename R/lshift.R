0c3fe3da1e5ef620da85a45880d967134b477f1c
fabnavarro <fabien.navarro@ensai.fr>
2018-03-07 15:30:14 +0100

    add example

--- R/lshift.R
+++ R/lshift.R
@@ -4,9 +4,8 @@
 #' @param a 1-d signal.
 #' @return \code{l} 1-d signal l(i) = x(i+1) except l(n) = x(1).
 #' @examples
-#' \dontrun{
+#' x <- MakeSignal('HeaviSine',2^3)
 #' lshift(x)
-#' }
 
 lshift <- function(a) {
   return(c(a[2:length(a)], a[1]))
