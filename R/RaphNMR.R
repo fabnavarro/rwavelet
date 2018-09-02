0815bac8885791b26541390f0b6774b831f57bb3
fabnavarro <fabien.navarro@ensai.fr>
2018-09-02 18:24:09 +0200

    add RaphNMR data description

---
+++ R/RaphNMR.R
@@ -0,0 +1,12 @@
+#' Nuclear magnetic resonance (NMR) signal.
+#'
+#' A dataset containing a NMR signal
+#'
+#' @format A numeric vector of length 1024
+#' @usage data(RaphNMR)
+#' @source MRS Unit, VA Medical Center, San Francisco. Adrain Maudsley, Ph.D., Professor of Radiology.
+#'     This NMR signal was obtained from Chris Raphael, then a
+#'     postdoctoral fellow in the Department of Statistics at
+#'     Stanford University who was working on Hidden Markov Models
+#'     for restoring NMR Spectra.
+"RaphNMR"
