0c98c8470bc118c35ecbd40daad52a0057642546
fabnavarro <fabien.navarro@ensai.fr>
2018-09-21 15:34:58 +0200

    spacing correction

--- R/UpSampleN.R
+++ R/UpSampleN.R
@@ -1,13 +1,11 @@
 #' Upsampling operator
 #'
-#'
 #' @export UpSampleN
 #' @param x 1-d signal, of length n.
 #' @param s upsampling scale, default = 2.
 #' @return \code{y} 1-d signal, of length s*n with zeros
 #'                  interpolating alternate samples
 #'                  y(s*i-1) = x(i), i=1,...,n
-
 
 UpSampleN <- function(x, s) {
   if (missing(s)) {
