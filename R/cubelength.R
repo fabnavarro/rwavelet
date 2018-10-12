09ec1d534eb88689489aad827c6d7e0a6ba0bbe8
fabnavarro <fabien.navarro@ensai.fr>
2018-10-12 23:35:45 +0200

    add cubelength for 3d transform

---
+++ R/cubelength.R
@@ -0,0 +1,17 @@
+cubelength <- function(x){
+	s <- dim(x)
+	n <- s[1]
+	if(s[2]!=s[1] | s[2]!=s[3]){
+		print('Warning nr!=nc or nr!=np')
+	}
+	k <- 1
+	J <- 0
+	while(k<n){
+		k <- 2*k
+		J <- J+1
+	}
+	if(k!=n){
+		print('Warning n!=2^J')
+	}
+	return(list(x=n,y=J))
+}
