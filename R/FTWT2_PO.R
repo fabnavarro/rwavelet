20685dc8c5c489963ec799a0f7dc3f326dab786e
fabnavarro <fabien.navarro@ensai.fr>
2018-10-20 10:20:51 +0200

    add 2-d tensor wavelet transform (periodized, orthogonal, FTWT2_PO)

---
+++ R/FTWT2_PO.R
@@ -0,0 +1,35 @@
+#' 2-d tensor wavelet transform (periodized, orthogonal).
+#'
+#' A two-dimensional Wavelet Transform is computed for the array x.
+#' \code{qmf} filter may be obtained from \code{\link{MakeONFilter}}.
+#' To reconstruct, use \code{\link{ITWT2_PO}}.
+#'
+#' @export FTWT2_PO
+#' @param x 2-d image (n by n array, n dyadic).
+#' @param L coarse level.
+#' @param qmf quadrature mirror filter.
+#' @return \code{wc} 2-d wavelet transform.
+#' @examples
+#' qmf <- MakeONFilter('Daubechies', 10)
+#' L <- 3
+#' x <- matrix(rnorm(128^2),ncol=128)
+#' wc <- FTWT2_PO(x, L, qmf)
+#' @seealso \code{\link{ITWT2_PO}}, \code{\link{MakeONFilter}}.
+
+FTWT2_PO <- function(x, L, qmf) {
+  q <- quadlength(x)
+  n <- q$x
+  J <- q$y
+  for (r in 1:n) {
+    row <- x[r, ]
+    wrow <- FWT_PO(row, L, qmf)
+    x[r, ] <- wrow
+  }
+  for (c in 1:n) {
+    col <- x[, c]
+    wcol <- FWT_PO(col, L, qmf)
+    x[, c] <- wcol
+  }
+  wc <- x
+  return(wc)
+}
