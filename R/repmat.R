20b67b4b64caca59a40a39c9d7fd5f36443d2de6
fabnavarro <fabien.navarro@ensai.fr>
2019-03-02 18:18:41 +0100

    add man repmat

--- R/repmat.R
+++ R/repmat.R
@@ -1,6 +1,10 @@
-repmat <- function(a, n, m){
-  a <- matrix(a,
-              nrow = 1,
-              ncol = length(a))
+#' Replicate and tile an array.
+#'
+#' @export repmat
+#' @param a input array (scalar, vector, matrix)
+#' @param n number of time to repeat input array in row and column dimensions
+#' @param m repetition factor
+repmat <- function(a, n, m) {
+  a <- matrix(a, nrow = 1, ncol = length(a))
   matrix(1, n, m) %x% a
 }
