8eb919701c743ab168aa6368545c360d2033dd7d
fabnavarro <fabien.navarro@ensai.fr>
2019-03-10 09:15:33 +0100

    add 3d Shepp Logan image

---
+++ R/3d_Shepp_Logan_phantom.R
@@ -0,0 +1,10 @@
+#' 3d Shepp-Logan phantom
+#'
+#' A dataset containing a 3d head phantom that can be used
+#' to test 3-D reconstruction algorithms.
+#' Shepp-Logan phantom is well-known imitation of human cerebral.
+#'
+#' @format A numeric array of size 64x64x64
+#' @usage data(SLphantom)
+#' @source \url{http://tomography.o-x-t.com/2008/04/13/3d-shepp-logan-phantom/}
+"SLphantom"
