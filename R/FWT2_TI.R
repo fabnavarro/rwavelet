1a4ff53bfc57067b472fd07fbd2c2297aa9fd4b5
fabnavarro <fabien.navarro@ensai.fr>
2019-03-14 13:33:31 +0100

    minor changes

--- R/FWT2_TI.R
+++ R/FWT2_TI.R
@@ -80,7 +80,7 @@ FWT2_TI <- function(x, L, qmf) {
   return(tiwt)
 }
 
-# Copyright (c) 1993. David L. Donoho
+# Copyright (c) 1995. David L. Donoho and Thomas P.Y. Yu
 
 # Part of Wavelab Version 850 Built Tue Jan 3 13:20:40 EST 2006 This is
 # Copyrighted Material For Copying permissions see COPYING.m Comments? e-mail
--- R/IWT2_TI.R
+++ R/IWT2_TI.R
@@ -53,3 +53,9 @@ IWT2_TI <- function(tiwt, L, qmf) {
   }
   return(x)
 }
+
+# Copyright (c) 1995. David L. Donoho and Thomas P.Y. Yu
+
+# Part of Wavelab Version 850 Built Tue Jan 3 13:20:40 EST 2006 This is
+# Copyrighted Material For Copying permissions see COPYING.m Comments? e-mail
+# wavelab@stat.stanford.edu
