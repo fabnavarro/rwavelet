61b9c0c3055da9a41e1057882d1dc546cfad9207
fabnavarro <fabien.navarro@ensai.fr>
2019-03-15 12:33:00 +0100

    added an example and description update

--- R/block_partition2d.R
+++ R/block_partition2d.R
@@ -1,7 +1,20 @@
 #' Construct 2d block partition
+#'
+#' Group the coefficients into blocks (or groups) of size L.
+#'
 #' @export block_partition2d
-#' @param x noisy wc at a given scale.
+#' @param x (noisy) wc at a given scale.
 #' @param L block size.
+#' @return \code{out} partition of coefficients by block.
+#' @examples
+#' x <- matrix(rnorm(2^2), ncol=2)
+#' j0 <- 0
+#' qmf <- MakeONFilter('Haar')
+#' wc <- FWT2_PO(x, j0, qmf)
+#' L <- 2
+#' wcb <- block_partition2d(wc, L)
+#' @seealso \code{\link{invblock_partition2d}}
+
 block_partition2d <- function(x, L) {
   n <- dim(x)[1]
   nblocks <- floor(n/L)
