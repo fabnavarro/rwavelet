e442f0e744e46dc45332c9e1081e611417e95491
fabnavarro <fabien.navarro@ensai.fr>
2019-03-15 12:26:24 +0100

    added an example and complement in the description

--- R/block_partition.R
+++ R/block_partition.R
@@ -1,7 +1,20 @@
 #' Construct 1d block partition
+#'
+#' This function is used to group the coefficients into blocks (or groups) of size L.
+#'
 #' @export block_partition
-#' @param x noisy wc at a given scale.
+#' @param x (noisy) wc at a given scale.
 #' @param L block size.
+#' @return \code{out} partition of coefficients by block.
+#' @examples
+#' x <- MakeSignal('Ramp', 8)
+#' j0 <- 0
+#' qmf <- MakeONFilter('Haar')
+#' wc <- FWT_PO(x, j0, qmf)
+#' L <- 2
+#' wcb <- block_partition(wc, L)
+#' @seealso \code{\link{invblock_partition}}, \code{\link{BlockThresh}}.
+
 block_partition <- function(x, L) {
   n <- length(x)
   nblocks <- floor(n/L)
