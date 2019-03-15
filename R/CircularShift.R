a8225995d7fb7eac5278f194f13140220306ed53
fabnavarro <fabien.navarro@ensai.fr>
2019-03-15 14:19:17 +0100

    description update

--- R/CircularShift.R
+++ R/CircularShift.R
@@ -1,6 +1,6 @@
-#' Circular Shifting of a matrix/image,
+#' Circular Shifting of a matrix/image
 #'
-#' pixels that get shifted off one side of the image
+#' Pixels that get shifted off one side of the image
 #' are put back on the other side.
 #'
 #' @export CircularShift
@@ -10,32 +10,31 @@
 #' @return \code{result} 2-d shifted signal.
 #' @examples
 #' A <- matrix(1:4, ncol=2, byrow=TRUE)
-#' CircularShift(A,0,-1)
+#' CircularShift(A, 0, -1)
+#' @seealso \code{\link{FWT2_TI}}, \code{\link{IWT2_TI}}.
 
 CircularShift <- function(matrix, colshift = 0, rowshift = 0) {
   lastrow <- nrow(matrix)
   lastcol <- ncol(matrix)
-  
   result <- matrix
-  
   # Shift the cols
   if (colshift > 0) {
-    result <- cbind(result[, (lastcol - colshift + 1):lastcol], result[, 
+    result <- cbind(result[, (lastcol - colshift + 1):lastcol], result[,
       seq_len(lastcol - colshift)])
   } else {
     colshift <- -colshift
-    result <- cbind(result[, seq(colshift + 1, lastcol, length = max(0, lastcol - 
-      colshift - 1 + 1))], result[, seq(1, colshift, length = max(0, colshift - 
+    result <- cbind(result[, seq(colshift + 1, lastcol, length = max(0, lastcol -
+      colshift - 1 + 1))], result[, seq(1, colshift, length = max(0, colshift -
       1 + 1))])
   }
   # Shift the rows
   if (rowshift > 0) {
-    result <- rbind(result[(lastrow - rowshift + 1):lastrow, ], result[seq_len(lastrow - 
+    result <- rbind(result[(lastrow - rowshift + 1):lastrow, ], result[seq_len(lastrow -
       rowshift), ])
   } else {
     rowshift <- -rowshift
-    result <- rbind(result[seq(rowshift + 1, lastrow, length = max(0, lastrow - 
-      rowshift - 1 + 1)), ], result[seq(1, rowshift, length = max(0, rowshift - 
+    result <- rbind(result[seq(rowshift + 1, lastrow, length = max(0, lastrow -
+      rowshift - 1 + 1)), ], result[seq(1, rowshift, length = max(0, rowshift -
       1 + 1)), ])
   }
   return(result)
