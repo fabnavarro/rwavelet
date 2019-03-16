eb0f84c48920517c34ff94a6ea532eb4c53b3f80
fabnavarro <fabien.navarro@ensai.fr>
2019-03-16 16:21:10 +0100

    description update

--- R/packet.R
+++ R/packet.R
@@ -1,9 +1,9 @@
-#' Packet table indexing.
+#' Packet table indexing
 #'
 #' @export packet
 #' @param d depth of splitting in packet decomposition.
 #' @param b block index among 2^d possibilities at depth d.
-#' @param n length of signal
+#' @param n length of signal.
 #' @return \code{p} linear indices of all coeff's in that block.
 #' @examples
 #' packet(1, 1, 8)
