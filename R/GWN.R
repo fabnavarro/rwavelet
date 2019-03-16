b22a3ed5fee34c6ccd7b8444f97f68b64325a8e9
fabnavarro <fabien.navarro@ensai.fr>
2019-03-16 15:55:23 +0100

    description update

--- R/GWN.R
+++ R/GWN.R
@@ -1,11 +1,11 @@
-#' Generation of Gaussian White Noise.
+#' Generation of Gaussian White Noise
 #'
 #' @export GWN
 #' @param n sample size.
 #' @param sigma standard deviation.
 #' @return \code{epsilon} resulting noise.
 #' @examples
-#' GWN(10,0.1)
+#' GWN(10, 0.1)
 
 GWN <- function(n, sigma) {
   epsilon <- rnorm(n = n, mean = 0, sd = sigma)
