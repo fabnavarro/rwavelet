52fa963b9038c43cf84c45c985fe270b86769681
fabnavarro <fabien.navarro@ensai.fr>
2019-03-16 16:34:12 +0100

    remove unnecessary coma

--- R/RaphNMR.R
+++ R/RaphNMR.R
@@ -3,7 +3,7 @@
 #' A dataset containing a NMR signal.
 #'
 #' @format A numeric vector of length 1024.
-#' @usage data(RaphNMR).
+#' @usage data(RaphNMR)
 #' @source MRS Unit, VA Medical Center, San Francisco. Adrain Maudsley, Ph.D., Professor of Radiology.
 #'     This NMR signal was obtained from Chris Raphael, then a
 #'     postdoctoral fellow in the Department of Statistics at
