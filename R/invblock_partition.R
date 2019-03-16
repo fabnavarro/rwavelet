ee82c5c3fb766e07c0cac6f8f52ca21b6214ac40
fabnavarro <fabien.navarro@ensai.fr>
2019-03-16 16:01:21 +0100

    add example

--- R/invblock_partition.R
+++ R/invblock_partition.R
@@ -1,8 +1,20 @@
 #' Inversion of the 1d block partition
+#'
 #' @export invblock_partition
 #' @param x noisy wc at a given scale.
 #' @param n scale.
 #' @param L block size.
+#' @examples
+#' n <- 8
+#' x <- MakeSignal('Ramp', n)
+#' j0 <- 1
+#' qmf <- MakeONFilter('Haar')
+#' wc <- FWT_PO(x, j0, qmf)
+#' L <- 2
+#' wcb <- block_partition(wc, L)
+#' wcib <- invblock_partition(wcb, n, L)
+#' @seealso \code{\link{block_partition}}, \code{\link{BlockThresh}}.
+
 invblock_partition <- function(x, n, L) {
   nblocks <- floor(n/L)
   buf <- matrix(x, c(L, nblocks))
