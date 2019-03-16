cba63a89668391a6a5e226d8560d6c1cb370307d
fabnavarro <fabien.navarro@ensai.fr>
2019-03-16 16:16:45 +0100

    description update

--- R/IWT3_PO.R
+++ R/IWT3_PO.R
@@ -1,10 +1,10 @@
-#' Inverse 3-d MRA wavelet transform (periodized, orthogonal).
+#' Inverse 3-d MRA Wavelet Transform (periodized, orthogonal)
 #'
-#' If \code{wc} is the result of a forward 3d wavelet transform, with \code{wc <- FWT3_PO(x, L, qmf)}.
+#' If \code{wc} is the result of a forward 3-d wavelet transform, with \code{wc <- FWT3_PO(x, L, qmf)}.
 #' then \code{x <- IWT3_PO(wc, L, qmf)} reconstructs \code{x} exactly
 #' \code{qmf} is a nice qmf, e.g. one made by \code{\link{MakeONFilter}}.
 #'
-#' 3-D counterpart of Donoho's IWT2_PO, original matlab code by
+#' 3-d counterpart of Donoho's IWT2_PO, original matlab code by
 #' Vicki Yang and Brani Vidakovic.
 #'
 #' @export IWT3_PO
@@ -32,19 +32,19 @@ IWT3_PO <- function(wc, L, qmf) {
     all <- 1:nc
     for (iy in 1:nc) {
       for (iz in 1:nc) {
-        x[all, iy, iz] <- UpDyadLo(x[bot, iy, iz], qmf) + UpDyadHi(x[top, 
+        x[all, iy, iz] <- UpDyadLo(x[bot, iy, iz], qmf) + UpDyadHi(x[top,
           iy, iz], qmf)
       }
     }
     for (ix in 1:nc) {
       for (iy in 1:nc) {
-        x[ix, iy, all] <- UpDyadLo(x[ix, iy, bot], qmf) + UpDyadHi(x[ix, 
+        x[ix, iy, all] <- UpDyadLo(x[ix, iy, bot], qmf) + UpDyadHi(x[ix,
           iy, top], qmf)
       }
     }
     for (ix in 1:nc) {
       for (iz in 1:nc) {
-        x[ix, all, iz] <- UpDyadLo(x[ix, bot, iz], qmf) + UpDyadHi(x[ix, 
+        x[ix, all, iz] <- UpDyadLo(x[ix, bot, iz], qmf) + UpDyadHi(x[ix,
           top, iz], qmf)
       }
     }
