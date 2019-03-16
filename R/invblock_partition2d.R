539fc52babbdc1eb8295115e6bda47e5a33a6e15
fabnavarro <fabien.navarro@ensai.fr>
2019-03-16 16:07:15 +0100

    add example

--- R/invblock_partition2d.R
+++ R/invblock_partition2d.R
@@ -1,8 +1,20 @@
 #' Inversion of the 2d block partition
 #' @export invblock_partition2d
-#' @param x noisy wc at a given scale.
+#' @param x partition of coefficients by block.
 #' @param n scale.
 #' @param L block size.
+#' @return \code{out} coefficients.
+#' @examples
+#' n <- 2
+#' x <- matrix(rnorm(n^2), ncol=2)
+#' j0 <- 0
+#' qmf <- MakeONFilter('Haar')
+#' wc <- FWT2_PO(x, j0, qmf)
+#' L <- 2
+#' wcb <- block_partition2d(wc, L)
+#' wcib <- invblock_partition2d(wcb, n, L)
+#' @seealso \code{\link{block_partition2d}}
+
 invblock_partition2d <- function(x, n, L) {
   nblocks <- floor(n/L)
   buf <- matrix(x, c(L, L * nblocks^2))
