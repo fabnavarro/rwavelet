b0cdca1dfcc11a664a972e2054be0b399b5f3554
fabnavarro <fabien.navarro@ensai.fr>
2019-03-16 16:26:14 +0100

    added example

--- R/repmat.R
+++ R/repmat.R
@@ -1,9 +1,13 @@
-#' Replicate and tile an array.
+#' Replicate and tile an array
+#'
+#' Repeat copies of array (equivalent of the repmat matlab function).
 #'
 #' @export repmat
-#' @param a input array (scalar, vector, matrix)
-#' @param n number of time to repeat input array in row and column dimensions
-#' @param m repetition factor
+#' @param a input array (scalar, vector, matrix).
+#' @param n number of time to repeat input array in row and column dimensions.
+#' @param m repetition factor.
+#' @examples
+#' repmat(10,3,2)
 repmat <- function(a, n, m) {
   a <- matrix(a, nrow = 1, ncol = length(a))
   matrix(1, n, m) %x% a
