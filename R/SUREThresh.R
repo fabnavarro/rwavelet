85e58f232bc04cdc746118ff8e6fa9e9bf0df375
fabnavarro <fabien.navarro@ensai.fr>
2019-03-20 17:21:02 +0100

    add SUREThresh fun

---
+++ R/SUREThresh.R
@@ -0,0 +1,19 @@
+#' Adaptive Threshold Selection Using Principle of SURE
+#'
+#' SURE referes to Stein's Unbiased Risk Estimate.
+#'
+#' @export SUREThresh
+#' @return y  Noisy Data with Std. Deviation = 1.
+#' @return thresh Threshold used.
+
+SUREThresh <- function(y) {
+  thresh <- ValSUREThresh(y)
+  x <- HardThresh(y, thresh)
+  
+}
+
+# Copyright (c) 1993-5.  Jonathan Buckheit, David Donoho and Iain Johnstone
+
+# Part of Wavelab Version 850 Built Tue Jan 3 13:20:39 EST 2006 This is
+# Copyrighted Material For Copying permissions see COPYING.m Comments? e-mail
+# wavelab@stat.stanford.edu
